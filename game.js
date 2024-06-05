6862ec47003a343bf2ef39e3753a39bdd9f4d112
Lukas <lukas.xiang0197@gmail.com>
2024-06-05 23:31:01 +0200

    changes

--- game.js
+++ game.js
@@ -50,4 +50,28 @@ $(function() {
             makeDrag(addImage("img/elements/Li.png", "li atom", "#game-window", "100px", "125px"));
         });
 
+        $("#btn-na").click(function() {
+            makeDrag(addImage("img/elements/Na.png", "na atom", "#game-window", "100px", "125px"));
+        });
+
+        $("#btn-k").click(function() {
+            makeDrag(addImage("img/elements/K.png", "k atom", "#game-window", "100px", "125px"));
+        });
+
+        $("#btn-cl").click(function() {
+            makeDrag(addImage("img/elements/Cl.png", "cl atom", "#game-window", "100px", "125px"));
+        });
+
+        $("#btn-f").click(function() {
+            makeDrag(addImage("img/elements/F.png", "f atom", "#game-window", "100px", "125px"));
+        });
+
+        $("#btn-br").click(function() {
+            makeDrag(addImage("img/elements/Br.png", "br atom", "#game-window", "100px", "125px"));
+        });
+
+        $("#btn-i").click(function() {
+            makeDrag(addImage("img/elements/I.png", "i atom", "#game-window", "100px", "125px"));
+        });
+
     });
